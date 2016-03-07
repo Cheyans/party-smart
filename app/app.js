51b2390f4bdf252594ba47ce211f8b6217cbfc05
cheyans <cheyan.set@gmail.com>
2016-03-07 15:23:27 -0500

    not working react rendering

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Router, Route, browserHistory } from 'react-router';
-// import HomePage from './components/homepage';
+import HomePage from './components/homepage';
 // import Dashboard from './components/dasbhoard';
 // import Complain from './components/complain';
 // import Host from './components/host';
@@ -15,14 +15,14 @@ class App extends React.Component {
   }
 }
 
-// ReactDOM.render((
-//   <Router history={browserHistory}>
-//     <Route path="/" component={App}>
-//       <IndexRoute component={HomePage} />
-//       <Route path = "/dashboard" component={Dashboard} />
-//       <Route path = "/complain" component={Complain} />
-//       <Route path = "/party-registration" component={Host} />
-//       <Route path = "/admin" component={Admin} />
-//     </Route>
-//   </Router>
-// ),document.getElementById('body.document'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage} />
+      // <Route path = "/dashboard" component={Dashboard} />
+      // <Route path = "/complain" component={Complain} />
+      // <Route path = "/party-registration" component={Host} />
+      // <Route path = "/admin" component={Admin} />
+    </Route>
+  </Router>
+),document.getElementById('body.document'));
--- app/components/homepage.js
+++ app/components/homepage.js
@@ -4,9 +4,41 @@ import Page from './page';
 
 export default class HomePage extends React.Component {
   render() {
+    var body = `
+    {<!-- Background Image -->}
+    <div class="background" />
+    <h2 class="title text">
+        Hosting parties made smart
+      </h2>
+    <div class="description text">
+      <p>
+        Register a party with us so that you are aware of the situation around you. If you feel like there is a party near you that is too loud you can file a complaint for receiving fast results.
+      </p>
+    </div>
+    <div class="panel panel-default container">
+      <div class="row panel-body services">
+        <div class="col-md-4">
+          <a class="btn service-button text" href="party-registration.html" role="button">
+            <span class="icon mdi mdi-file-document" aria-hidden="true"></span>
+            <h4 class="service-text">Register a Party</h4>
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a class="btn service-button text" href="complain.html" role="button">
+            <span class="icon mdi mdi-comment-alert" aria-hidden="true"></span>
+            <h4 class="service-text">File a Complaint</h4>
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a class="btn service-button text" href="#" role="button">
+            <span class="icon mdi mdi-account-multiple-plus" aria-hidden="true"></span>
+            <h4 class="service-text">Request to Attend a Party</h4>
+          </a>
+        </div>
+      </div>
+    </div>`
     return (
-      <Page user={1}/>
-      
+      <Page user={1} body={body} header="" title="Party Smart"/>
     )
   }
 }
