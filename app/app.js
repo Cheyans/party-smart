99d10a53a882b63ecfa3140be8d9eebd77651a42
vladislavapaskova <pasko22v@mtholyoke.edu>
2016-03-07 00:08:57 -0500

    worked with Cheyan to edit some of the features of the homepage

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Router, Route, browserHistory } from 'react-router';
-import HomePage from './components/homepage';
-import Dashboard from './components/dasbhoard';
-import Complain from './components/complain';
-import Host from './components/host';
-import Admin from './components/admin';
+// import HomePage from './components/homepage';
+// import Dashboard from './components/dasbhoard';
+// import Complain from './components/complain';
+// import Host from './components/host';
+// import Admin from './components/admin';
 
 class App extends React.Component {
   render() {
--- app/components/homepage.js
+++ app/components/homepage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router';
-import Page from 'page';
+import Page from './page';
 
 export default class HomePage extends React.Component {
   render() {
     return (
-      <Page user={1}><Page/>
+      <Page user={1}/>
       
     )
   }
