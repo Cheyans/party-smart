2cc10948a372d27ec9a5fd7996f88e72e6615511
cheyans <cheyan.set@gmail.com>
2016-03-07 20:51:55 -0500

    partially working index

--- app/app.js
+++ app/app.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {IndexRoute, Router, Route, browserHistory} from 'react-router';
-import App from './components/app';
-import HomePage from './components/homepage';
-// import Dashboard from './components/dasbhoard';
+import Page from './components/page';
+import Index from './components/index';
 // import Complain from './components/complain';
 // import Host from './components/host';
 // import Admin from './components/admin';
 
+class Test extends React.Component {
+  render() {
+    return (
+      <p>test</p>
+    )
+  }
+}
+
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={HomePage}/>
+    <Route path="/" component={Page}>
+      <IndexRoute component={Index}/>
+      <Route path="admin" component={Test} />
     </Route>
   </Router>
 ), document.getElementById('app'));
