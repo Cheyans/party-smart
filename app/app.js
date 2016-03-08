4fc3635d8dc3d33b32e8f2337afb7f6853ca4e12
vladislavapaskova <pasko22v@mtholyoke.edu>
2016-03-07 20:52:54 -0500

    created complain.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import {IndexRoute, Router, Route, browserHistory} from 'react-router';
 import App from './components/app';
 import HomePage from './components/homepage';
 // import Dashboard from './components/dasbhoard';
-// import Complain from './components/complain';
+import Complain from './components/complain';
 // import Host from './components/host';
 // import Admin from './components/admin';
 
@@ -12,6 +12,7 @@ ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={HomePage}/>
+      <Route path="admin" component={Complain}/>
     </Route>
   </Router>
 ), document.getElementById('app'));
