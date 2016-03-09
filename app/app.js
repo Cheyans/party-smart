9c99e7c1f073d1f7131930cf849f723b72162ae0
Mira Murali <mural22m@mtholyoke.edu>
2016-03-08 22:46:25 -0500

    edited app.js with it routing to host a party

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {IndexRoute, Router, Route, browserHistory} from 'react-router';
 import Page from './components/page';
 import Index from './components/index';
+import Host from './components/host';
 // import Complain from './components/complain';
 // import Host from './components/host';
 // import Admin from './components/admin';
@@ -11,6 +12,7 @@ ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={Page}>
       <IndexRoute component={Index}/>
+      <Route path="host" component={Host}/>
     </Route>
   </Router>
 ), document.getElementById('app'));
