299e5782db9a2f2a1363a6f78c96360bb6f7b9ae
alexredden00@gmail.com <alexredden00@gmail.com>
2016-03-12 11:44:05 -0500

    merge fix

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {IndexRoute, Router, Route, browserHistory} from 'react-router';
-import Index from './components/index';
 import Page from './components/page';
+import IndexPage from './components/index';
 import Profile from './components/profile';
-import PartyInfo from './components/party-info';
+import PartyInfo from './components/party-info.js';
 // import Complain from './components/complain';
 // import Host from './components/host';
 import AdminPage from './components/admin.js';
@@ -12,8 +12,8 @@ import AdminPage from './components/admin.js';
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={Page}>
-      <IndexRoute component={Index}/>
-      <Route path= "/profile/:id" component={Profile}></Route>
+      <IndexRoute component={IndexPage}/>
+      <Route path= "/profile/:id" component={Profile}/>
       <Route path="admin" component={AdminPage}></Route>
       <Route path="party" component={PartyInfo}></Route>
     </Route>
