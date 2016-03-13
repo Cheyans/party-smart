b7a12d9c8f50d4c5e96c4dfa88d6eb57f67874d3
cheyans <cheyan.set@gmail.com>
2016-03-13 13:36:52 -0400

    partially implemented clear all fields and small cleanup

--- app/components/host.js
+++ app/components/host.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {createNewParty} from '../server';
 import Item from './host-item';
 
+var inputs;
+
 export default class Host extends React.Component {
   constructor(props) {
     super(props);
@@ -14,8 +16,8 @@ export default class Host extends React.Component {
     };
   }
 
-  postParty() {
-    var inputs = [
+  componentDidMount() {
+    inputs = [
       document.getElementById('title'),
       document.getElementById('description'),
       document.getElementById('address'),
@@ -25,9 +27,18 @@ export default class Host extends React.Component {
       document.getElementById('country'),
       document.getElementById('time'),
       document.getElementById('date')
-      //document.getElementById('invite'),
-      //document.getElementById('supplies')
     ];
+  }
+
+  clearFields() {
+    for (var input of inputs) {
+      input.value = "";
+    }
+    document.getElementById('invited').value = "";
+    document.getElementById('supplies').value = "";
+  }
+
+  postParty() {
     var newParty = {};
     for (var input of inputs) {
       if (input.value != "") {
@@ -86,72 +97,72 @@ export default class Host extends React.Component {
   render() {
     return (
       <div className="host">
-        <div className="container">
-          <div className="panel panel-default">
-            <div className="panel-body">
-              <fieldset>
-                <div className="row row-padding">
-                  <div className='form-group col-lg-12 title'>
-                    <input className='form-control' placeholder="Party Name" required="true" type="text" id="title"/>
+      <div className="container">
+        <div className="panel panel-default">
+          <div className="panel-body">
+            <fieldset>
+              <div className="row row-padding">
+                <div className='form-group col-lg-12 title'>
+                  <input className='form-control' placeholder="Party Name" required="true" type="text" id="title"/>
+                </div>
+              </div>
+              <div className="row row-padding">
+                <div className='form-group col-lg-12 partyName'>
+                  <input className='form-control' placeholder="Description" required="true" type="text" id="description"/>
+                </div>
+              </div>
+              <div className="row row-padding">
+                <div className='form-group col-lg-11 address'>
+                  <input className="form-control" placeholder="Address" required="true" type="text" id="address"/>
+                </div>
+                <div className="col-lg-1 autolocate">
+                  <button type="button" className="btn btn-default">
+                    <span className="mdi mdi-map-marker"></span>
+                  </button>
+                </div>
+              </div>
+              <div className='row row-padding'>
+                <div className='col-sm-4'>
+                  <div className='form-group'>
+                    <input className="form-control" placeholder="City" required="true" type="text" id="city"/>
                   </div>
                 </div>
-                <div className="row row-padding">
-                  <div className='form-group col-lg-12 partyName'>
-                    <input className='form-control' placeholder="Description" required="true" type="text" id="description"/>
+                <div className='col-sm-2'>
+                  <div className='form-group'>
+                    <input className="form-control" placeholder="State / Province" required="true" type="text" id="state"/>
                   </div>
                 </div>
-                <div className="row row-padding">
-                  <div className='form-group col-lg-11 address'>
-                    <input className="form-control" placeholder="Address" required="true" type="text" id="address"/>
-                  </div>
-                  <div className="col-lg-1 autolocate">
-                    <button type="button" className="btn btn-default">
-                      <span className="mdi mdi-map-marker"></span>
-                    </button>
+                <div className='col-sm-2'>
+                  <div className='form-group'>
+                    <input className="form-control" placeholder="Postal or Zip Code" required="true" type="text" id="zip"/>
                   </div>
                 </div>
-                <div className='row row-padding'>
-                  <div className='col-sm-4'>
-                    <div className='form-group'>
-                      <input className="form-control" placeholder="City" required="true" type="text" id="city"/>
-                    </div>
-                  </div>
-                  <div className='col-sm-2'>
-                    <div className='form-group'>
-                      <input className="form-control" placeholder="State / Province" required="true" type="text" id="state"/>
-                    </div>
-                  </div>
-                  <div className='col-sm-2'>
-                    <div className='form-group'>
-                      <input className="form-control" placeholder="Postal or Zip Code" required="true" type="text" id="zip"/>
-                    </div>
-                  </div>
-                  <div className='col-sm-4'>
-                    <div className='form-group'>
-                      <input className="form-control" placeholder="Country" required="true" type="text" id="country"/>
-                    </div>
+                <div className='col-sm-4'>
+                  <div className='form-group'>
+                    <input className="form-control" placeholder="Country" required="true" type="text" id="country"/>
                   </div>
                 </div>
-                <div className='row row-padding'>
-                  <div className='col-sm-6'>
-                    <div className='form-group'>
-                      <input className="form-control" placeholder="Time" required="true" type="text" id="time"/>
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <div className='form-group'>
-                      <input className="form-control" placeholder="Date" required="true" type="text" id="date"/>
-                    </div>
+              </div>
+              <div className='row row-padding'>
+                <div className='col-sm-6'>
+                  <div className='form-group'>
+                    <input className="form-control" placeholder="Time" required="true" type="text" id="time"/>
                   </div>
                 </div>
-                <div className='row row-padding'>
-                  <div className='col-sm-6'>
-                <div className="form-group">
-                  <input className="form-control" placeholder="Who do you want to invite?" required="true" type="text" id="invited"/>
-                  <a className="btn btn-default col-lg-6" href="#" role="button" onClick={() => this.addInvitee()}>
-                    Add Person
-                  </a>
+                <div className='col-sm-6'>
+                  <div className='form-group'>
+                    <input className="form-control" placeholder="Date" required="true" type="text" id="date"/>
+                  </div>
                 </div>
+              </div>
+              <div className='row row-padding'>
+                <div className='col-sm-6'>
+                  <div className="form-group">
+                    <input className="form-control" placeholder="Who do you want to invite?" required="true" type="text" id="invited"/>
+                    <a className="btn btn-default col-lg-6" href="#" role="button" onClick={() => this.addInvitee()}>
+                      Add Person
+                    </a>
+                  </div>
                 </div>
 
                 <div className="col-md-6">
@@ -165,56 +176,57 @@ export default class Host extends React.Component {
                 </div>
 
               </div>
-              </fieldset>
+            </fieldset>
 
-                <img className="col-lg-12 map" src="img/map.jpg" width="100%"/>
-                <div className="col-lg-12">
-                  <div className="col-md-6">
+            <img className="col-lg-12 map" src="img/map.jpg" width="100%"/>
+            <div className="col-lg-12">
+              <div className="col-md-6">
 
-                     <table className="table table-striped">
-                       <thead>
-                         <tr>
-                           <th>Currently Invited</th>
-                         </tr>
-                       </thead>
-                       <tbody>
-                         {this.state.currentlyInvited.map((invited, i) => {
-                           debugger;
-                           return <Item key={i} name={invited} delete={this.deleteItem} type="invite"></Item>
-                         })}
-                       </tbody>
-                     </table>
-                   </div>
-
-                    <div className="col-md-6">
-                      <table className="table table-striped">
-                      <thead>
-                        <tr>
-                          <th>Supplies Requested</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {this.state.suppliesRequested.map((invited, i) => {
-                          return <Item key={i} name={invited} delete={this.deleteItem()} type="supply"></Item>
-                        })}
-                      </tbody>
-                    </table>
-                      </div>
-                  </div>
-
-
-                <div className="col-lg-12">
-                  <a className="btn btn-default col-lg-9" href="#" role="button" onClick={() => this.postParty()}>
-                    Create Party
-                  </a>
-                  <a className="btn btn-default col-lg-3" href="#" role="button">
-                    Clear All Fields
-                  </a>
-                </div>
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Currently Invited</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      {this.state.currentlyInvited.map((invited, i) => {
+                        return <Item key={i} name={invited} delete={this.deleteItem} type="invite"></Item>
+                      })}
+                    </tr>
+                  </tbody>
+                </table>
               </div>
+
+              <div className="col-md-6">
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Supplies Requested</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      {this.state.suppliesRequested.map((invited, i) => {
+                        return <Item key={i} name={invited} delete={this.deleteItem()} type="supply"></Item>
+                      })}
+                    </tr>
+                </tbody>
+              </table>
             </div>
           </div>
+
+          <div className="col-lg-12">
+            <a className="btn btn-default col-lg-9" href="#" role="button" onClick={() => this.postParty()}>
+              Create Party
+            </a>
+            <a className="btn btn-default col-lg-3" href="#" role="button" onClick={() => this.clearFields()}>
+              Clear All Fields
+            </a>
+          </div>
         </div>
+      </div>
+    </div> < /div>
 
     )
   }
