d52430ded159c2c82c6bf7f42e82b1b65bf12bd6
cheyans <cheyan.set@gmail.com>
2016-04-02 15:03:52 -0400

    add common server functions

--- server/src/server.js
+++ server/src/server.js
@@ -10,25 +10,64 @@ var addDocument = database.addDocument;
 var app = express();
 app.use(bodyParser.text());
 app.use(bodyParser.json());
-
 app.use(express.static('../../client/build'));
 
+// Fetch user id
+app.get(`/users/:id`, undefined, function(req, res) {
+  var userIdRequesting = getUserIdFromToken(req.get('Authorization'));
+  var userIdRequested = parseInt(req.params.userid);
+  if(userIdRequested === userIdRequesting) {
 
+  }
+});
 
-// app.use(function(err, req, res, next) {
-//   if (err.name === 'JsonSchemaValidation') {
-//     res.status(400).end();
-//   } else {
-//     next(err);
-//   }
-// });
+function getBasicUserInfo(userId) {
+  var user = readDocument('users', userId);
+  return {
+    id: user._id,
+    fname: user.fname,
+    lname: user.lname,
+    picture: user.picture
+  }
+}
+
+/**
+ * Get the user ID from a token. Returns -1 (an invalid ID) if it fails.
+ */
+function getUserIdFromToken(authorizationLine) {
+  try {
+    // Cut off "Bearer " from the header value.
+    var token = authorizationLine.slice(7);
+    // Convert the base64 string to a UTF-8 string.
+    var regularString = new Buffer(token, 'base64').toString('utf8');
+    // Convert the UTF-8 string into a JavaScript object.
+    var tokenObj = JSON.parse(regularString);
+    var id = tokenObj['id'];
+    // Check that id is a number.
+    if (typeof id === 'number') {
+      return id;
+    } else {
+      // Not a number. Return -1, an invalid ID.
+      return -1;
+    }
+  } catch (e) {
+    // Return an invalid ID.
+    return -1;
+  }
+}
+
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    res.status(400).end();
+  } else {
+    next(err);
+  }
+});
 
 // Reset database.
 app.post('/resetdb', function(req, res) {
   console.log("Resetting database...");
-  // This is a debug route, so don't do any validation.
   database.resetDatabase();
-  // res.send() sends an empty response with status code 200
   res.send();
 });
 
